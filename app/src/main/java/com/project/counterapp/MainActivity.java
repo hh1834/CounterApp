b5c09fa029972bb21236b645fcbea72ade2f5e56
Hansol Hong <hh1834@nyu.edu>
2023-06-17 14:58:54 -0500

    Add views and button for the counter

--- app/src/main/java/com/project/counterapp/MainActivity.java
+++ app/src/main/java/com/project/counterapp/MainActivity.java
@@ -3,12 +3,22 @@ package com.project.counterapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView titleView;
+    TextView counterView;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        titleView = findViewById(R.id.title);
+        counterView = findViewById(R.id.counterView);
+        button = findViewById(R.id.button1);
     }
 }
