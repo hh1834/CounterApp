bcccf91c5b319952aaea719f0a9b701945c4a25c
Hansol Hong <hh1834@nyu.edu>
2023-06-17 15:06:28 -0500

    Increase counter on button click

--- app/src/main/java/com/project/counterapp/MainActivity.java
+++ app/src/main/java/com/project/counterapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.project.counterapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -12,13 +13,24 @@ public class MainActivity extends AppCompatActivity {
     TextView counterView;
     Button button;
 
+    int counter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        counter = 0;
 
         titleView = findViewById(R.id.title);
         counterView = findViewById(R.id.counterView);
         button = findViewById(R.id.button1);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                counter += 1;
+                counterView.setText("" + counter);
+            }
+        });
     }
 }
